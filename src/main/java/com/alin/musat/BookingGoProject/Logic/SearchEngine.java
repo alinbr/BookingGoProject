fdf435b22fc4034d63441dbc37ae5f3993f6e957
Alin-Gabriel Musat <alin_musat_96@yahoo.com>
2018-10-11 14:36:32 +0100

    Added structure to SearchEngine class.

--- src/main/java/com/alin/musat/BookingGoProject/Logic/SearchEngine.java
+++ src/main/java/com/alin/musat/BookingGoProject/Logic/SearchEngine.java
@@ -2,18 +2,39 @@ package com.alin.musat.BookingGoProject.Logic;
 
 import com.alin.musat.BookingGoProject.Models.GeoLocation;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Main part of application.
+ */
 public class SearchEngine {
+
+
+    private List<String> supplierEndPoints = new ArrayList<>();
+
 
     public SearchEngine() {}
 
 
-    public void addSupplier(String supplier) {
-
-    }
-
+    /**
+     * Use this method to perform a new search.
+     * @param pickUp Pick up geolocation
+     * @param dropOff Drop off geolocation
+     * @param numberOfPassengers Number of passengers.
+     * @return Query response in for of a SearchResponse object.
+     */
     public SearchResponse newSearch(GeoLocation pickUp, GeoLocation dropOff, int numberOfPassengers) {
         return null;
     }
 
+
+    public void addSupplier(String endPoint) {
+        supplierEndPoints.add(endPoint);
+    }
+
+    public void removeSupplier(String endPoint) {
+        supplierEndPoints.removeIf(s -> s.equals(endPoint));
+    }
 
 }
